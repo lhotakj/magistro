be55c03f294139aa7adde0beaa4b8abbb3f7c7da
Jaroslav Lhotak <lhotakj@gmail.com>
2023-12-03 13:08:42 +0100

    time challenges, colors, and new soudns added

--- main.py
+++ main.py
@@ -4,33 +4,48 @@ import random
 import os
 import time
 import datetime
+import re
+from colorama import Fore, Style
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from pygame import mixer
 
+TIME1_QUESTIONS: list = [
+    {"How many hours are in a day?": "24"},
+    {"How many hours are in two days?": "48"},
+    {"How many days are in a week?": "7"},
+    {"How many days are in a two weeks?": "14"},
+    {"How many days are in a three weeks?": "21"},
+    {"How many days are in a four weeks?": "28"},
+    {"How many weeks are in a month?": "4"},
+    {"How many months are in a year?": "12"},
+    {"How many days are in a leap year?": "366"},
+    {"How many days are in a normal year?": "365"},
+    {"What is the name of the shortest month of the year?": ["feb", "february"]},
+    {"How many hours are in half a day?": "12"},
+    {"How many minutes are in an hour?": "60"},
+    {"How many seconds are in a minute?": "60"},
+]
 
-def play_success():
+
+def escape_ansi(line):
+    ansi_escape = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
+    return ansi_escape.sub('', line)
+
+
+def play(sound: str) -> None:
     mixer.init()
-    mixer.music.load('./sounds/success.mp3')
+    mixer.music.load(f"./sounds/{sound}.mp3")
     mixer.music.play()
 
 
-def play_intro():
-    mixer.init()
-    mixer.music.load('./sounds/intro.mp3')
-    mixer.music.play()
-
-
-def play_outro():
-    mixer.init()
-    mixer.music.load('./sounds/outro.mp3')
-    mixer.music.play()
-
-
-def play_fail():
-    mixer.init()
-    mixer.music.load('./sounds/fail.mp3')
-    mixer.music.play()
+def time1() -> tuple[list[str], str]:
+    problems: list = TIME1_QUESTIONS
+    problem: dict = problems[random.randrange(0, len(problems) - 1)]
+    question: str = list(problem.keys())[0] + " "
+    answers: str | list = list(problem.values())[0]
+    answers = answers if isinstance(answers, list) else [answers]
+    return answers, question
 
 
 def addition() -> tuple[str, str]:
@@ -75,37 +90,52 @@ def print_log(text: str, new_line: bool = True, std_out: bool = True):
     if std_out:
         print(text)
     with open("./results.txt", "a") as file:
-        file.write(text + os.linesep if new_line else '')
+        file.write(escape_ansi(text) + os.linesep if new_line else '')
 
 
 def teacher():
-    play_intro()
+    print(Style.RESET_ALL)
+    play("intro")
     print_log("===========================")
-    print_log(f"Welcome {os.getenv('USER', '')}, get ready for solving {TOTAL_PROBLEMS} math "
-              f"problems using {', '.join(OPERATIONS)}. Good luck ")
-    print_log("Started at " + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print_log(f"Welcome {os.getenv('USER', '')}, get ready for solving {Fore.GREEN}{TOTAL_PROBLEMS}{Style.RESET_ALL} "
+              f"problems using {', '.join(OPERATIONS)}. Good luck!")
+    print_log("Started at " + Fore.GREEN + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + Style.RESET_ALL)
     print_log("---------------------------")
     start = time.time()
     failed = 0
     attempts = 0
+    play("start")
     for x in range(0, TOTAL_PROBLEMS):
+        if x == TOTAL_PROBLEMS:
+            play("final")
         result, problem_text = eval(random.choice(OPERATIONS) + "()")
         ok: bool = False
+        problem_text = ("#" + str(x + 1) + "/" + str(TOTAL_PROBLEMS) + ": ").ljust(
+            8) + Fore.LIGHTWHITE_EX + problem_text + Style.RESET_ALL
+
         while not ok:
             attempts += 1
             print_log(text=problem_text, new_line=False, std_out=False)
-            user_input = input(problem_text)
+            user_input = input(problem_text).lower()
             print_log(text=problem_text + user_input, new_line=True, std_out=False)
-            if user_input != result:
-                failed += 1
-                play_fail()
+            if isinstance(result, list):
+                if user_input not in result:
+                    failed += 1
+                    play("fail")
+                else:
+                    play("success")
+                    break
             else:
-                play_success()
-                break
+                if user_input != result:
+                    failed += 1
+                    play("fail")
+                else:
+                    play("success")
+                    break
 
     end = time.time()
 
-    play_outro()
+    play("outro")
 
     print_log("---------------------------")
     print_log(f"Total elapsed time (hh:mm:ss.ff) {format(format_time_elapsed(end - start))}")
@@ -117,8 +147,9 @@ def teacher():
     print_log("===========================")
 
 
-TOTAL_PROBLEMS: int = 15
-OPERATIONS: list = ['addition', 'subtraction', 'multiplication', 'division']
+TOTAL_PROBLEMS: int = 2
+# OPERATIONS: list = ['addition', 'subtraction', 'multiplication', 'division']
+OPERATIONS: list = ['time1']
 ADD_SUB_MAX_RESULT: int = 100
 ADD_SUB_MIN_NUMBER: int = 30
 ADD_SUB_MAX_NUMBER: int = 90
